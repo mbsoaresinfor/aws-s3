bcdf84624d40bd074e185177742f32d5207fcad5
Marcelo Soares <marcelo@rodosoft.com.br>
2023-05-24 14:42:19 -0300

    cont. project

--- src/main/java/br/com/aws/s3/AwsS3Application.java
+++ src/main/java/br/com/aws/s3/AwsS3Application.java
@@ -2,12 +2,57 @@ package br.com.aws.s3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.ListObjectsRequest;
+import software.amazon.awssdk.services.s3.model.ListObjectsResponse;
+import software.amazon.awssdk.services.s3.model.S3Exception;
+import software.amazon.awssdk.services.s3.model.S3Object;
+
+import java.util.List;
 
 @SpringBootApplication
 public class AwsS3Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AwsS3Application.class, args);
+		//SpringApplication.run(AwsS3Application.class, args);
+		String bucketName = "marcelo-test-bucket"; // tem que estar criado o bucket na aws
+
+		
+		Region region = Region.SA_EAST_1;
+		S3Client s3 = S3Client.builder()
+				.region(region)
+				.build();
+
+		listBucketObjects(s3, bucketName);
+		s3.close();
+	}
+
+	public static void listBucketObjects(S3Client s3, String bucketName ) {
+
+		try {
+			ListObjectsRequest listObjects = ListObjectsRequest
+					.builder()
+					.bucket(bucketName)
+					.build();
+
+			ListObjectsResponse res = s3.listObjects(listObjects);
+			List<S3Object> objects = res.contents();
+			for (S3Object myValue : objects) {
+				System.out.print("\n The name of the key is " + myValue.key());
+				System.out.print("\n The object is " + calKb(myValue.size()) + " KBs");
+				System.out.print("\n The owner is " + myValue.owner());
+			}
+
+		} catch (S3Exception e) {
+			System.err.println(e.awsErrorDetails().errorMessage());
+			System.exit(1);
+		}
+	}
+
+	private static long calKb(Long val) {
+		return val/1024;
 	}
 
 }
